1546fdef22ff9f58c874b23b7e291972112550ae
PMachete2005 <154671193+PMachete2005@users.noreply.github.com>
2026-06-11 17:05:29 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,49 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mySet = set()
+        minimum = 10000000000
+        for n in nums:
+            if n in mySet:
+                continue
+            else:
+                if minimum > n:
+                    minimum = n
+                mySet.add(n)
+        consecutivetemp = 0
+        consecutive = 0
+        mintemp = minimum
+        while mintemp in mySet:
+            mySet.remove(mintemp)
+            mintemp += 1
+            consecutivetemp += 1
+            if mintemp not in mySet:
+                if consecutivetemp > consecutive:
+                    consecutive = consecutivetemp
+                    consecutivetemp = 0
+                else:
+                    consecutivetemp = 0 
+                temp = 10000000000
+                for x in mySet:
+                    if x > mintemp and x < temp:
+                        temp = x
+                mintemp = temp
+        return consecutive
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
