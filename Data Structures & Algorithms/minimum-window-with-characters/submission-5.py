39f276b7c52efdbd4f9f304f8adffc06c5098ab2
PMachete2005 <154671193+PMachete2005@users.noreply.github.com>
2026-08-01 12:16:41 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,57 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        left = 0 
+        minlength = float('inf')
+        minstr = ""
+        sMap = {}
+        tMap = {}
+        for i in range(len(t)):
+            if t[i] in tMap:
+                tMap[t[i]] += 1 
+            else:
+                tMap[t[i]] = 1
+            if s[i] in sMap:
+                sMap[s[i]] += 1
+            else:
+                sMap[s[i]] = 1
+        print(sMap)
+        print(tMap)
+        matches = 0 
+        for ele in tMap:
+            if ele in sMap and tMap[ele] <= sMap[ele]:
+                matches += 1
+        if len(s) == len(t):
+            for i in range(len(s)):
+                if s[i] not in tMap or sMap[s[i]] != tMap[s[i]]:
+                    return ""
+            return s
+        print(matches)
+        if matches == len(t):
+            return s[left:left + len(t)]
+        right = left + len(t)
+        while right < len(s):
+            if s[right] in sMap:
+                sMap[s[right]] += 1
+            else:
+                sMap[s[right]] = 1
+            if s[right] in tMap:
+                if tMap[s[right]] == sMap[s[right]]:
+                    print(sMap)
+                    matches += 1
+                    print(matches)
+            if matches == len(tMap):
+                while matches == len(tMap):
+                    if s[left] in tMap and (sMap[s[left]] - 1) < tMap[s[left]]:
+                        if len(s[left:right + 1]) < minlength:
+                            minlength = len(s[left:right + 1])
+                            minstr = s[left:right + 1]
+                            print(minstr)
+                        matches -= 1
+                    sMap[s[left]] -= 1
+                    print("In left increment loop",sMap)
+                    left += 1
+            right += 1 
+        return minstr
+            
