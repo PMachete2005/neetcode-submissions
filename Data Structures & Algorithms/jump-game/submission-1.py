95672828c893c05727f68a12db30221b59961670
PMachete2005 <154671193+PMachete2005@users.noreply.github.com>
2026-08-12 10:56:41 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        j = goal - 1
+        while j >= 0:
+            if nums[j] >= (goal - j):
+                goal = j
+            j -= 1
+        if goal == 0:
+            return True 
+        else:
+            return False
